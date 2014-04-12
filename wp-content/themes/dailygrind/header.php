849a2427919c1e1a6cae0dfb10df2a3d5757536f
mattSOLANO <msolano@HQMACDESIGN.local>
2014-04-12 15:40:07 -0400

    Favicon

--- wp-content/themes/dailygrind/header.php
+++ wp-content/themes/dailygrind/header.php
@@ -14,6 +14,8 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
 
 <?php wp_head(); ?>
 
